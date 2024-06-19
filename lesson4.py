57ab059a2a50deae2d97243f453823d4f532c7ea
nikoloz <167115744+nikolozkakabadze123@users.noreply.github.com>
2024-06-19 08:02:45 -0700

    Update lesson4.py

--- lesson4.py
+++ lesson4.py
@@ -3,3 +3,100 @@ last_name = 'kakabadze'
 hobi = 'programing'
 country = 'georgia'
 print(name + last_name + hobi + country)
+
+
+
+# Codes with +
+print(15 + 25)
+print(30 + 40)
+print(110 + 60)
+print(10 + 10)
+print(30 + 40)
+print(60 + 80)
+print(20 + 15)
+print(35 + 20)
+print(100 + 20)
+print(65 + 55)
+print(12 + 8)
+print(22 + 28)
+print(105 + 45)
+print(11 + 5)
+print(35 + 15)
+
+
+
+
+# Codes with -
+print(55 - 35)
+print(110 - 30)
+print(85 - 50)
+print(25 - 5)
+print(60 - 40)
+print(90 - 50)
+print(50 - 30)
+print(70 - 60)
+print(110 - 50)
+print(46 - 26)
+print(60 - 40)
+print(115 - 40)
+print(90 - 55)
+print(30 - 10)
+print(65 - 45)
+
+
+
+# Codes with *
+print(7 * 6)
+print(15 * 10)
+print(8 * 7)
+print(6 * 2)
+print(12 * 5)
+print(25 * 3)
+print(10 * 8)
+print(15 * 9)
+print(35 * 2)
+print(9 * 10)
+print(6 * 5)
+print(12 * 8)
+print(7 * 6)
+print(5 * 3)
+print(10 * 4)
+
+
+
+
+# Codes with //
+print(90 // 6)
+print(70 // 7)
+print(160 // 8)
+print(30 // 3)
+print(75 // 5)
+print(105 // 7)
+print(55 // 5)
+print(80 // 10)
+print(120 // 8)
+print(88 // 11)
+print(85 // 5)
+print(65 // 6)
+print(150 // 10)
+print(25 // 2)
+print(80 // 4)
+
+
+
+# Codes with **
+print(2 ** 4)
+print(5 ** 3)
+print(8 ** 2)
+print(4 ** 3)
+print(3 ** 5)
+print(7 ** 2)
+print(6 ** 4)
+print(9 ** 3)
+print(10 ** 2)
+print(12 ** 2)
+print(3 ** 4)
+print(6 ** 3)
+print(9 ** 2)
+print(5 ** 4)
+print(4 ** 2)
